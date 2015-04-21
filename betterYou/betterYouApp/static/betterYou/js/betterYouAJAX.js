837280e789c2a2864c287a1823b3c7bc9ae79083
zacharilius <zabensley@gmail.com>
2015-04-21 09:31:17 -0400

    AJAX working for vote template

--- betterYou/betterYouApp/static/betterYou/js/betterYouAJAX.js
+++ betterYou/betterYouApp/static/betterYou/js/betterYouAJAX.js
@@ -1,11 +1,9 @@
-$(document).ready(function() {
-$('#likes').click(function() {
-	console.log("Fired")
+$(document).delegate('#likes', 'click', function() {
 	var challid;
 	challid = $(this).attr("data-challengeid");
 	$.get('/projects/better-you/like-challenge/', {challenge_id: challid}, function(data){
-		$('#vote_count').html(data);
-		$('#likes').hide();
+		console.log(data);
+		$("[data-vote=" + challid + "]").html(data)
 	});
-});
+	$(this).hide();
 });
